881a605e72fbb5ce2f61f0ff6a9d02f8cd2b8bb8
Julian Benavies <ajbenc@gmail.com>
2025-02-19 23:20:04 -0500

    [frontend] modifications for the cloudflare pages deployment

--- ezshop/vite.config.ts
+++ ezshop/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // Ensure this matches your build output directory
+    sourcemap: true, // Optional: Enable sourcemaps for debugging
+  },
+});
